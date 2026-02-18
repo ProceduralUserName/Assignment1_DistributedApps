e11fca02de2d263b771c96ab1c86d615472787c7
Simon <127345492+ProceduralUserName@users.noreply.github.com>
2026-02-17 19:51:54 -0500

    getbyid, create, update status,delete

--- SB_VehicleInventoryMicroservice/Controllers/SB_VehiclesController.cs
+++ SB_VehicleInventoryMicroservice/Controllers/SB_VehiclesController.cs
@@ -22,5 +22,47 @@ namespace SB_VehicleInventoryMicroservice.Controllers
             var vehicles = await _vehicleService.GetAllVehiclesAsync();
             return Ok(vehicles);
         }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SB_VehicleDto>> GetById(Guid id)
+        {
+            var vehicle = await _vehicleService.GetVehicleByIdAsync(id);
+            if (vehicle == null)
+                return NotFound();
+
+            return Ok(vehicle);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<SB_VehicleDto>> Create(SB_CreateVehicleDto dto)
+        {
+            var vehicle = await _vehicleService.CreateVehicleAsync(dto);
+            return CreatedAtAction(nameof(GetById), new { id = vehicle.Id }, vehicle);
+        }
+
+        [HttpPut("{id}/status")]
+        public async Task<ActionResult<SB_VehicleDto>> UpdateStatus(Guid id, SB_UpdateVehicleStatusDto dto)
+        {
+            try
+            {
+                var vehicle = await _vehicleService.UpdateVehicleStatusAsync(id, dto);
+                return Ok(vehicle);
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+            catch (SB_InvalidVehicleStateException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            await _vehicleService.DeleteVehicleAsync(id);
+            return NoContent();
+        }
     }
 }
