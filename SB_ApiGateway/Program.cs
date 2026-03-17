70f153380f50f519bd4f8b1d09d8b651a943fdef
Simon <127345492+ProceduralUserName@users.noreply.github.com>
2026-03-17 14:31:13 -0400

    modifying settings / regsitering middleware in program.cs

--- SB_ApiGateway/Program.cs
+++ SB_ApiGateway/Program.cs
@@ -1,6 +1,15 @@
+using SB_ApiGateway.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseMiddleware<SB_GatewayExceptionMiddleware>();
+app.UseMiddleware<SB_ApiKeyMiddleware>();
+
+app.MapReverseProxy();
 
 app.Run();
